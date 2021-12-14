061758220bcd591271aecda38c5cdad95b45a83c
armin <2730083511@qq.com>
2021-12-14 20:03:06 +0800

    Method2

--- src/main/java/com/arminzheng/concurrent/senior/CompletableFutureApi.java
+++ src/main/java/com/arminzheng/concurrent/senior/CompletableFutureApi.java
@@ -16,10 +16,32 @@ public class CompletableFutureApi {
 
     public static void main(String[] args) {
         try {
-            ApiI();
+            ApiII();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 对计算结果进行处理
+     */
+    public static void ApiII() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+                    return 1;
+                }, threadPoolExecutor)
+                .thenApply(f -> f + 1)
+                .thenApply(f -> f + 2)
+                .whenComplete((v, e) -> {
+                    if (e == null) {
+                        System.out.println("result: " + v);
+                    }
+                }).exceptionally(e -> {
+                    e.printStackTrace();
+                    return null;
+                });
+        System.out.println(future.join());
+        threadPoolExecutor.shutdown();
     }
 
     /**
@@ -30,7 +52,7 @@ public class CompletableFutureApi {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             try { TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) { e.printStackTrace();}
             return 1;
-        },threadPoolExecutor);
+        }, threadPoolExecutor);
         // System.out.println(future.get());
         // System.out.println(future.get(2L,TimeUnit.SECONDS)); // 过时不候，会抛 TimeoutException 异常
         // System.out.println(future.getNow(9999)); // 给一个默认值，不抛异常
