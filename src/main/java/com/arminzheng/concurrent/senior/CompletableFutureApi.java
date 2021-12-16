0f321db5085fdf4662dedf6bef939d2f3f8ee271
zhengy <zhengy>
2021-12-16 13:57:16 +0800

    微调顺序

--- src/main/java/com/arminzheng/concurrent/senior/CompletableFutureApi.java
+++ src/main/java/com/arminzheng/concurrent/senior/CompletableFutureApi.java
@@ -24,29 +24,7 @@ public class CompletableFutureApi {
     }
 
     /**
-     * 对计算结果进行处理
-     */
-    public static void ApiII() {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
-        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
-                    return 1;
-                }, threadPoolExecutor)
-                .thenApply(f -> f + 1)
-                .thenApply(f -> f + 2)
-                .whenComplete((v, e) -> {
-                    if (e == null) {
-                        System.out.println("result: " + v);
-                    }
-                }).exceptionally(e -> {
-                    e.printStackTrace();
-                    return null;
-                });
-        System.out.println(future.join());
-        threadPoolExecutor.shutdown();
-    }
-
-    /**
-     * 获得结果和触发计算
+     * 1. 获得结果和触发计算
      *
      * exceptionally -> try/catch
      * whenComplete + handle -> try/finally
@@ -67,6 +45,28 @@ public class CompletableFutureApi {
         // System.out.println(future.getNow(9999)); // 给一个默认值，不抛异常
         // System.out.println(future.join()); // 和get一样，但不会抛异常
         System.out.println("打断是否成功：" + future.complete(8888) + "\t返回结果为" + future.get()); // 主动触发计算，返回是否打断
+        threadPoolExecutor.shutdown();
+    }
+
+    /**
+     * 2. 对计算结果进行处理
+     */
+    public static void ApiII() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+                    return 1;
+                }, threadPoolExecutor)
+                .thenApply(f -> f + 1)
+                .thenApply(f -> f + 2)
+                .whenComplete((v, e) -> {
+                    if (e == null) {
+                        System.out.println("result: " + v);
+                    }
+                }).exceptionally(e -> {
+                    e.printStackTrace();
+                    return null;
+                });
+        System.out.println(future.join());
         threadPoolExecutor.shutdown();
     }
 
