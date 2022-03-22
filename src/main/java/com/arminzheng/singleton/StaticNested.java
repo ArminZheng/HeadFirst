9019cb77c0f88cb6367dde130fbc07c6d36e1c67
armin <arminzheng@qq.com>
2022-03-22 13:37:10 +0800

    单例模式

---
+++ src/main/java/com/arminzheng/singleton/StaticNested.java
@@ -0,0 +1,26 @@
+package com.arminzheng.singleton;
+
+/**
+ * 延迟初始化占位类
+ *
+ * @author zy
+ * @version 2022/3/22
+ */
+public class StaticNested {
+
+  public static class StaticNestedHolder {
+    // JVM将推迟内部类的初始化操作，直到开始使用这个类时才初始化
+    public static final StaticNested INSTANCE = new StaticNested();
+  }
+
+  private StaticNested() {
+  }
+
+  public static StaticNested getInstance() {
+    return StaticNestedHolder.INSTANCE;
+  }
+
+  public void dumb() {
+    System.out.println("dumb dumb");
+  }
+}
