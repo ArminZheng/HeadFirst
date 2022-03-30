8aa99628d115a98df644bc52714272eec5fdfba8
zhengy <zhengy>
2022-03-30 14:31:26 +0800

    适配器模式一定要继承目标来，然后构造器组合现有类

--- src/main/java/com/arminzheng/adapter/DuckTestDrive.java
+++ src/main/java/com/arminzheng/adapter/DuckTestDrive.java
@@ -3,18 +3,24 @@ package com.arminzheng.adapter;
 import com.arminzheng.strategy.Duck;
 import com.arminzheng.strategy.ModelDuck;
 
+/**
+ * 适配器模式 = 继承 + 组合
+ *
+ * <p>简单来说，继承目标，组合现有类（使用构造器）
+ */
 public class DuckTestDrive {
     public static void main(String[] args) {
-        ModelDuck modelDuck = new ModelDuck();
-
         WildTurkey wildTurkey = new WildTurkey();
-        Duck turkey = new TurkeyAdapter(wildTurkey);
         System.out.println("test Turkey says...");
         wildTurkey.gobble();
         wildTurkey.fly();
+
         System.out.println(">>>>>>>>>\ntest TurkeyAdapter says...");
+        Duck turkey = new TurkeyAdapter(wildTurkey);
         testDuck(turkey);
-        System.out.println(">>>>>>>>>\ntest Duck says...");
+
+        System.out.println(">>>>>>>>>\ntest ModelDuck says...");
+        ModelDuck modelDuck = new ModelDuck();
         testDuck(modelDuck);
     }
 
