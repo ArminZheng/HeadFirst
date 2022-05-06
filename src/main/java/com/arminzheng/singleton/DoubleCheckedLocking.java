d14fda490dccd40b47bb3d29931ea0286ce7f2bf
armin <arminzheng@qq.com>
2022-05-07 00:53:35 +0800

    双重检测锁 补充 DCL

--- src/main/java/com/arminzheng/singleton/DoubleCheckedLocking.java
+++ src/main/java/com/arminzheng/singleton/DoubleCheckedLocking.java
@@ -8,7 +8,6 @@ package com.arminzheng.singleton;
  */
 public class DoubleCheckedLocking {
 
-    private volatile static DoubleCheckedLocking uniqueInstance;
     /*
      volatile 的原因：禁止指令重排序
       new SomeObject() equals:
@@ -18,8 +17,18 @@ public class DoubleCheckedLocking {
       2、3步可能出现重排序，也就是分配好空间后，直接将对象指向还未实例化的地址，导致其他对象判断不为空，拿到对象进行操作就会报错。
   （还有解决可见性的作用）
       多个线程将同一个数据读取到各自的缓存区后，某个cpu修改了缓存的数据之后，会立马同步给主存，volatile就是这么实现可见性的
-     */
 
+      May, 7 2022 new: 分配好内存空间后，初始化对象 和 指向刚分配的内存空间 容易乱序
+        导致：指向内存空间后，导致B以为拿到初始化后的对象，使用时导致空指针异常
+        修复：通过 volatile 关键字，指令重排序被禁止，就让B知道访问的是一个初始化未完成的对象。
+        @ volatile屏蔽指令重排序的语义在JDK1.5中才被完全修复，仍然不能完全避免重排序，JDK1.5之前的Java中无法安全地使用DCL。
+     */
+    private volatile static DoubleCheckedLocking uniqueInstance;
+    public String here;
+
+    /**
+     * 双重检查锁的原因：只加一层会导致每次进来都加锁，而实际上只在第一次初始化的时候才需要用到，之后的调用都没必要再进行加锁。
+     */
     private DoubleCheckedLocking() {
     }
 
@@ -36,12 +45,6 @@ public class DoubleCheckedLocking {
         // 除此之外，还有eagerly实例化和使用同步的getInstance
     }
 
-    public String here;
-
-    public void boil() {
-        System.out.println("is boil" + here);
-    }
-
     public static void main(String[] args) {
         DoubleCheckedLocking instance = DoubleCheckedLocking.getInstance();
         instance.boil();
@@ -53,5 +56,11 @@ public class DoubleCheckedLocking {
         instance.here = "stock fish";
         instanceI.boil();
         instanceII.boil();
+        DoubleCheckedLocking instanceIII = DoubleCheckedLocking.getInstance();
+        instanceIII.boil();
+    }
+
+    public void boil() {
+        System.out.println("is boil: " + here);
     }
 }
